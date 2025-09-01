82b0dbdb2abc91b2aafd7ca91718b3206227a960
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-09-01 11:14:48 -0300

    feat: adiciona arquivos docker para rodar

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 4174,
+    strictPort: true,
+  },
 });
